7b14980c326104282f9cdf9edaf91ccb709f690b
sasha4ox <sasha4ox@gmail.com>
2019-05-29 17:39:08 +0300

    fix bugs

--- js.js
+++ js.js
@@ -1,16 +1,18 @@
-var imgFlagU = document.getElementById("imgFlagU");
-var startTime = Date.now();
-
-var timer = setInterval(function(){
-    var TimePassed = Date.now() - startTime;
-    if (TimePassed >= 5000){
-        clearInterval(timer);
-        return;
-    };
-    changeOpacity(TimePassed);
-}, 100 );
-
-
-function changeOpacity(TimePassed){
-    imgFlagU.style.opacity = (TimePassed/10000) + 0.6;
+window.onload = function(){
+    var imgFlagU = document.getElementById("imgFlagU");
+    var startTime = Date.now();
+    
+    var timer = setInterval(function(){
+        var TimePassed = Date.now() - startTime;
+        if (TimePassed >= 5000){
+            clearInterval(timer);
+            return;
+        };
+        changeOpacity(TimePassed);
+    }, 100 );
+    
+    
+    function changeOpacity(TimePassed){
+        imgFlagU.style.opacity = (TimePassed/10000) + 0.6;
+    }
 }
